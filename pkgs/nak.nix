adcb28d7704b16de5d76d15184da025ba668032d
jurraca <julienu@pm.me>
2026-06-01 12:15:04 +0000

    nak: 0.15.3 -> 0.19.10; add fuse2 and C linker env vars

--- pkgs/nak.nix
+++ pkgs/nak.nix
@@ -1,17 +1,22 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, fuse2 }:
 
 buildGoModule rec {
   pname = "nak";
-  version = "0.15.3";
+  version = "0.19.10";
 
   src = fetchFromGitHub {
     owner = "fiatjaf";
     repo = "nak";
     rev = "v" + version;
-    sha256 = "sha256-PSg+27uTpPIrKlYArWOv92l5muQRQiFZ6Vvu7hDLt5s=";
+    sha256 = "sha256-7j9O8SAig3OMdvtVsxP9Ar1CjUWOhFovKo63S5IbNf8=";
   };
 
-  vendorHash = "sha256-qwi3awU1DHjT/4scGUrhsdlmXJYwq0g/t4LaZ8FGYB0=";
+  vendorHash = "sha256-Eeg49ida69AUY5viTHHNgiL8wTXtXRG3kTMiCrU6zCY=";
+  buildInputs = [ fuse2 ];
+  env = {
+    CGO_LDFLAGS = "-L${lib.getLib fuse2}/lib -lfuse";
+    CGO_CFLAGS = "-I${lib.getDev fuse2}/include";
+  };
 
   doCheck = false;
 
