249f8a8344a5d3a2c43790445ab039413c8cde7c
jurraca <julienu@pm.me>
2026-07-21 16:47:36 +0200

    haven: 1.0.4 -> 1.2.2; update new repo owner

--- pkgs/haven.nix
+++ pkgs/haven.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "haven";
-  version = "1.0.4";
+  version = "1.2.2";
 
   src = fetchFromGitHub {
-    owner = "bitvora";
+    owner = "barrydeen";
     repo = "haven";
     rev = "v" + version;
-    sha256 = "sha256-/0j8BP7NQAqKt/1SXcPI565+YCAxuI4Kn/rSDW57oS8=";
+    sha256 = "sha256-gpfTgaO3VK65GBy/W/rR8181yHlvgTx9UyWReo7s2gQ=";
   };
 
-  vendorHash = "sha256-YqXzlNIXGg8a9cw4qxGGs4UYs5PnKhXQxlziSIFcDc0=";
+  vendorHash = "sha256-VXx6uoOUKk/BkjDS3Ykf/0Xc2mUPm8dgyRArIb2I8X4=";
 
   doCheck = false;
 
